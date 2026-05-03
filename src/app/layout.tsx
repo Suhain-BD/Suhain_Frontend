dc840d57627840b0e76d4a58cfdf41a96e95da5e
niloy8 <niloybhuiyan688@gmail.com>
2026-05-03 16:22:26 +0600

    Layout font updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist_Mono, Geist } from "next/font/google";
 import "./globals.css";
-
 const geistSans = Geist({
-  variable: "--font-geist-sans",
   subsets: ["latin"],
+  // weight: ["400", "500", "600", "700"],
 });
-
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+  // variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
@@ -25,7 +23,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.className} ${geistMono.className} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
